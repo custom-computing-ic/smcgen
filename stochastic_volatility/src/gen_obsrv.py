12144d71680c174f4cb6ac42e46e7e13533fa6f3
thomasccp <thomasccp@msn.com>
2014-02-24 21:25:25 +0000

    Tidy up

--- stochastic_volatility/src/gen_obsrv.py
+++ stochastic_volatility/src/gen_obsrv.py
@@ -3,7 +3,6 @@ from math import *
 
 fx = open('data_x.txt','w')
 fy = open('data_y.txt','w')
-fo = open('obsrv.txt','w')
 
 x = 0
 for i in range(10):
@@ -12,7 +11,6 @@ for i in range(10):
     print 'x: {0} y: {1}'.format(x, y)
     fx.write(str(x)+'\n')
     fy.write(str(y)+'\n')
-    fo.write(str(y)+'\n')
 
 fx.close()
 fy.close()
