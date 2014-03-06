a452e2397e4d103ff2aedb1e389b880b8bf548e2
thomasccp <thomasccp@msn.com>
2014-03-06 17:07:40 +0000

    Updated initialisation

--- robot_localisation_with_moving_objects/src/Func.c
+++ robot_localisation_with_moving_objects/src/Func.c
@@ -285,8 +285,8 @@ void init(int NP, int slotOfAllP, char* obsrvFile, float* obsrv, char* refFile, 
 			state[p*SS+1] = ((float) dsfmt_genrand_close_open(&dsfmt[p]))*12;
 			state[p*SS+2] = ((float) dsfmt_genrand_close_open(&dsfmt[p]))*2*Pi;
 		}else{ // particles of the moving objects
-			state[p*SS] = ((float) dsfmt_genrand_close_open(&dsfmt[p]))*18;
-			state[p*SS+1] = ((float) dsfmt_genrand_close_open(&dsfmt[p]))*12;
+			state[p*SS] = 8+((float) dsfmt_genrand_close_open(&dsfmt[p]))*(18-8);
+			state[p*SS+1] = 6+((float) dsfmt_genrand_close_open(&dsfmt[p]))*(12-6);
 			state[p*SS+2] = ((float) dsfmt_genrand_close_open(&dsfmt[p]))*2*Pi;
 		}
 	}
