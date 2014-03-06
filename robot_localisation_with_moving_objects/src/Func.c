148351b9374616e0b4a3b21362836d3dc2be3846
thomasccp <thomasccp@msn.com>
2014-03-06 12:38:33 +0000

    Working on the Kernel

--- robot_localisation_with_moving_objects/src/Func.c
+++ robot_localisation_with_moving_objects/src/Func.c
@@ -342,8 +342,9 @@ void check(char *stateFile){
 		fscanf(fpXest, "%f %f %f\n", &x_est, &y_est, &h_est);
 		step_error = sqrt(pow(x_est-x,2)+pow(y_est-y,2));
 		total_error += step_error;
-	}
+	
 	printf("Average error: %f\n", fabs(total_error)/(NT*1.0));
+	printf("Time: %f\n", itl_outer*(NP*slotOfP/(NC*150000000.0)+((NP*SlotOfP*SS+NP*NPObj)*sizeof(float))/2000000000.0));
 	fclose(fpX);
 	fclose(fpXest);
 
